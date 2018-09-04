1dcf678d37589cfa35e7c518a07979f56a1d0741
Thomas Nigro <thomas@opuscope.com>
2018-09-04 11:56:08 +0200

    Initial commit: contains Unity and VS DLL .NET 2 sample

---
+++ Assets/EFCoreTest.cs
@@ -0,0 +1,19 @@
+﻿using DLLs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EFCoreTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Test test = new Test();
+        test.Strings.Add("coucou");
+        test.SaveChanges();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ VS_DLLs/DLLs/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DLLs
+{
+    public  static class Class1
+    {
+        static Class1()
+        {
+            string url = "Hello";
+            HttpClient client = new HttpClient();
+            var str = client.GetStringAsync(url);
+        }
+    }
+
+    public class Test : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            InMemoryDbContextOptionsExtensions.UseInMemoryDatabase(optionsBuilder);
+        }
+
+        public DbSet<string> Strings { get; set; }
+    }
+}
